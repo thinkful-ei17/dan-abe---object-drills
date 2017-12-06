298c4da1d29804f9535464c0274cf4694b364648
Dan Rosen <drosen84@gmail.com>
2017-12-06 16:24:59 -0600

    pseudo code for dbsearch

---
+++ dbsearch.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query) {
+/*loop through each object within the array
+for each object:    
+    - make sure each key in the query matches the object
+    - if it does, return it
+    - if not, keep looping
+
+    */
+}
+findOne(HEROES, { id: 5, squad: 'Justice League' });
