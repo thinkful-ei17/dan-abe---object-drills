c5366305423016f6717106b0b6de20ea05c126d9
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-06 11:30:45 -0800

    create arrays-in-objects

---
+++ arrays-in-objects.js
@@ -0,0 +1,12 @@
+const hobbit = {
+    meals: ['breakfast', 
+    'second braekfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+]
+}
+
+console.log(hobbit.meals[3]);
