b8ef66f147aab8a0187942c3f4ec6825d573cb48
Dan Rosen <drosen84@gmail.com>
2017-12-06 11:17:54 -0600

    added objdrills2.js and did student report exercise

---
+++ objdrills2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function makeStudentsReport(data) {
+  return data.map(student => `${student.name} : ${student.grade}`);
+}
+
+const testData = [
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' },
+];
+
+console.log(makeStudentsReport(testData));
