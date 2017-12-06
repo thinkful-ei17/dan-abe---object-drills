950fd7913c879b178cd15da53bde63cecd4dbafe
Dan Rosen <drosen84@gmail.com>
2017-12-06 11:08:33 -0600

    added self-reference drill

--- objectdrills.js
+++ objectdrills.js
@@ -15,11 +15,24 @@ console.log(createMyObject());
 const newObj = createMyObject();
 
 function updateObject(obj){
-    obj.foo = 'foo';
-    obj.bar = 'bar';
-    obj.bizz = 'bizz';
-    obj.bang = 'bang';
-    return obj;
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
 }
 
 console.log(updateObject(newObj));
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {return this.firstName + " " + this.lastName}
+  };
+  return person;
+}
+const myPerson = personMaker();
+console.log(myPerson.fullName());
