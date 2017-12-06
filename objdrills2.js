e602c45bbb06f3acce85880c0a8efb2ea825cb39
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-06 09:23:03 -0800

    Add enrollInSummerSchool function

--- objdrills2.js
+++ objdrills2.js
@@ -11,3 +11,30 @@ const testData = [
 ];
 
 console.log(makeStudentsReport(testData));
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students){
+    for (let i = 0; i < students.length; i++){
+        students[i].status = 'In Summer School';
+    }
+    return students;
+}
+
+console.log(enrollInSummerSchool(studentData));
